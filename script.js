405c3af2043a7aba5417c4340d79a241bf1fa312
anav5704 <anavkki@gmail.com>
2025-03-23 10:40:18 +1300

    feat: add navbar active state

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Best Health")
+
+const links = document.querySelectorAll(".nav-link");
+
+links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(".active")?.classList.remove("active");
+        this.classList.add("active");
+    });
+});
